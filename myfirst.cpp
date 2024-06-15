f343d148488359c6314de8bf90816dfdf02ca48f
yuno1346 <yuno1346@gmail.com>
2024-06-15 15:16:35 +0900

    test commit

---
+++ myfirst.cpp
@@ -0,0 +1,13 @@
+// my first.cpp -- output
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Welcome to C++ world!!";
+    cout << endl;
+    cout << "dont worry" << endl;
+
+    return 0;
+}
