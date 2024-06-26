8a72ac390339964ec2ffb7e13c922f1e0b50d7b2
yuno1346 <yuno1346@gmail.com>
2024-06-26 21:50:14 +0900

    3

--- myfirst.cpp
+++ myfirst.cpp
@@ -4,7 +4,7 @@
 
 int main()                                   // function header
 {                                            // start of function body
-    using namespace std;                     //  make definitions visible
+    using namespace std;                     // make definitions visible
     cout << "come up and C++ me some time."; // message
     cout << endl;                            // start a new line
     cout << "You won't regret it!" << endl;  // more output
