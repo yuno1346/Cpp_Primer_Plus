6019d70fe678565bb57c9d9da3be8717839e50ac
Kim Yunho <135622856+yuno1346@users.noreply.github.com>
2025-03-05 23:20:21 +0900

    250305

---
+++ 3_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int n;
+    const int ratio = 10;
+    cout << "Enter a length(cm): ";
+    cin >> n;
+    cout << "Length in meters: " << n / ratio << endl;
+    return 0;
+}
